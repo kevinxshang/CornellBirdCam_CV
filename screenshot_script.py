c3861f2ed4e8b11aee80d3bcaa7acf1c37dd6c71
Bella Hu <brh76@cornell.edu>
2024-03-18 19:48:58 -0400

    modification to run gpt model at all times but save to dataframe at the end

--- screenshot_script.py
+++ screenshot_script.py
@@ -9,60 +9,10 @@ import requests
 import os
 from os import listdir
 import pandas as pd
+import os.path
 
-# Specify the path to Chromedriver
-# Specify bird cam URL
-# Specify iterations
-# Specify screenshots frequency
-# Adjust if necessary 
-chromedriver_path = './chromedriver-mac-arm64/chromedriver'
-cam_url = 'https://www.youtube.com/watch?v=NgZNlISgfLE'
-iterations_count = 5
-screenshots_frequency = 5
-
-# Specify Chrome Options for headless
-options = webdriver.ChromeOptions()
-# Headless mode
-options.add_argument('--headless') 
-options.add_argument('--disable-extensions')
-options.add_argument('--disable-gpu')
-options.add_argument('--no-sandbox') 
-options.add_argument('--disable-dev-shm-usage')
- # Autoplay videos
-options.add_argument('--autoplay-policy=no-user-gesture-required')
-
-# Set up the Chrome service and initialize the Chrome driver
-service = Service(executable_path=chromedriver_path)
-driver = webdriver.Chrome(service=Service(executable_path=chromedriver_path), options=options)
-driver.get(cam_url)
-
-# Wait for the live video to load
-time.sleep(5)
-
-try:
-    '''
-    # Will use this only with GUI (not headless)
-
-    # Attempt to play the video by simulating a spacebar press
-    driver.find_element(By.TAG_NAME, 'body').send_keys(Keys.SPACE) 
-    time.sleep(2)
-    
-    # Attempt to go full screen by clicking the full screen button
-    fullscreen_button = driver.find_element(By.CSS_SELECTOR, 'button.ytp-fullscreen-button')
-    fullscreen_button.click()
-    time.sleep(2)
-    '''
-    
-    for _ in range(iterations_count):
-        # Take and save with "shots_frequency" pause time
-        timestamp = int(time.time())
-        date_time_format = datetime.fromtimestamp(timestamp).strftime('%Y-%m-%d %H:%M:%S')
-        driver.save_screenshot(f'screenshots/screenshot_{date_time_format}.png')
-        time.sleep(screenshots_frequency)
-    
-finally:
-    # Clean up and close the browser window
-    driver.quit()
+screenshot_names = []
+results = []
 
 def encode_image(image_path):
   with open(image_path, "rb") as image_file:
@@ -110,14 +60,73 @@ def process_image_and_prompt(input_image=None):
         return content
     else:
         return f"Error: {response.status_code} - {response.reason}"
+
+# Specify the path to Chromedriver
+# Specify bird cam URL
+# Specify iterations
+# Specify screenshots frequency
+# Adjust if necessary 
+chromedriver_path = './chromedriver-mac-arm64/chromedriver'
+cam_url = 'https://www.youtube.com/watch?v=Mez4F1iFxxM&ab_channel=CornellLabBirdCams'
+iterations_count = 5
+screenshots_frequency = 5
+
+# Specify Chrome Options for headless
+options = webdriver.ChromeOptions()
+# Headless mode
+options.add_argument('--headless') 
+options.add_argument('--disable-extensions')
+options.add_argument('--disable-gpu')
+options.add_argument('--no-sandbox') 
+options.add_argument('--disable-dev-shm-usage')
+ # Autoplay videos
+options.add_argument('--autoplay-policy=no-user-gesture-required')
+
+# Set up the Chrome service and initialize the Chrome driver
+service = Service(executable_path=chromedriver_path)
+driver = webdriver.Chrome(service=Service(executable_path=chromedriver_path), options=options)
+driver.get(cam_url)
+
+# Wait for the live video to load
+time.sleep(5)
+
+try:
+    '''
+    # Will use this only with GUI (not headless)
+
+    # Attempt to play the video by simulating a spacebar press
+    driver.find_element(By.TAG_NAME, 'body').send_keys(Keys.SPACE) 
+    time.sleep(2)
     
-DIR = "./screenshots"
-df = pd.DataFrame(columns = ["screenshot", "bird_tf"])
-screenshot_names = []
-results = []
-for img in os.listdir(DIR):
-    screenshot_names.append(img)
-    results.append(process_image_and_prompt(f"./screenshots/{img}"))
-df["screenshot"] = screenshot_names
-df["bird_tf"] = results
-df.to_csv("data.csv")
+    # Attempt to go full screen by clicking the full screen button
+    fullscreen_button = driver.find_element(By.CSS_SELECTOR, 'button.ytp-fullscreen-button')
+    fullscreen_button.click()
+    time.sleep(2)
+    '''
+    
+    for _ in range(iterations_count):
+        # Take and save with "shots_frequency" pause time
+        timestamp = int(time.time())
+        date_time_format = datetime.fromtimestamp(timestamp).strftime('%Y-%m-%d %H:%M:%S')
+        img_name = f'screenshots/screenshot_{date_time_format}.png'
+        driver.save_screenshot(img_name)
+        time.sleep(screenshots_frequency)
+        screenshot_names.append(img_name)
+        results.append(process_image_and_prompt(img_name))
+    
+finally:
+    # Clean up and close the browser window
+    driver.quit()
+
+DIR = "data.csv"
+if os.path.isfile(DIR):
+    df = pd.read_csv(DIR)
+    df_new = pd.DataFrame({"screenshots" : screenshot_names, "bird_tf": results})
+    data = pd.concat([df, df_new])
+else:
+    data = pd.DataFrame({"screenshots" : screenshot_names, "bird_tf": results})
+
+# for img in os.listdir(DIR):
+#     screenshot_names.append(img)
+#     results.append(process_image_and_prompt(f"./screenshots/{img}"))
+data.to_csv("data.csv")
