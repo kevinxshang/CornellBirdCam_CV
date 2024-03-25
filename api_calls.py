4d1c02f858095d9bc9ec60fed5aec08757710d51
Bella Hu <brh76@cornell.edu>
2024-03-25 19:04:57 -0400

    made more cost efficient

--- api_calls.py
+++ api_calls.py
@@ -1,4 +1,4 @@
-from screenshot_script import take_screenshot
+# from screenshot_script import take_screenshot
 import requests
 import os
 from os import listdir
@@ -6,7 +6,7 @@ import pandas as pd
 import os.path
 import base64
 
-# Key here
+# key here
 
 def encode_image(image_path):
   with open(image_path, "rb") as image_file:
@@ -54,37 +54,3 @@ def process_image_and_prompt(input_image=None):
         return content
     else:
         return f"Error: {response.status_code} - {response.reason}"
-    
-
-if __name__ == "__main__":
-  chromedriver_path = './chromedriver-mac-arm64/chromedriver'
-  cam_url = 'https://www.youtube.com/watch?v=Mez4F1iFxxM&ab_channel=CornellLabBirdCams'
-  iterations_count = 5
-  screenshots_frequency = 5
-  if not os.path.exists('screenshots'):
-    os.makedirs('screenshots')
-  take_screenshot(cam_url, chromedriver_path, iterations_count, screenshots_frequency)  
-
-  screenshot_dir = 'screenshots'
-  screenshot_names = [os.path.join(screenshot_dir, img) for img in os.listdir(screenshot_dir)]
-  results = [process_image_and_prompt(img) for img in screenshot_names]
-
-
-  DIR = "data.csv"
-  if os.path.isfile(DIR):
-      df = pd.read_csv(DIR)
-      df_new = pd.DataFrame({"screenshots" : screenshot_names, "bird_tf": results})
-      data = pd.concat([df, df_new])
-  else:
-      data = pd.DataFrame({"screenshots" : screenshot_names, "bird_tf": results})
-
-  # for img in os.listdir(DIR):
-  #     screenshot_names.append(img)
-  #     results.append(process_image_and_prompt(f"./screenshots/{img}"))
-  data.to_csv("data.csv")
-
-  df = pd.read_csv("data.csv")
-
-  df = df.loc[:, ~df.columns.str.contains('^Unnamed')]
-
-  df.to_csv("data_new.csv", index=False)
--- screenshot_script.py
+++ screenshot_script.py
@@ -5,14 +5,17 @@ from selenium.webdriver.common.keys import Keys
 import time
 from datetime import datetime
 import base64
-
+from api_calls import process_image_and_prompt
 import os
 from os import listdir
 import pandas as pd
 import os.path
 
-def take_screenshot(cam_url, chromedriver_path, iterations_count, screenshots_frequency):
-  screenshot_names = []
+screenshot_names = []
+results = []
+
+def take_screenshot(df, cam_url, chromedriver_path, iterations_count, screenshots_frequency):
+
   # Specify Chrome Options for headless
   options = webdriver.ChromeOptions()
   # Headless mode
@@ -54,9 +57,46 @@ def take_screenshot(cam_url, chromedriver_path, iterations_count, screenshots_fr
         driver.save_screenshot(img_name)
         time.sleep(screenshots_frequency)
         screenshot_names.append(img_name)
-        # results.append(process_image_and_prompt(img_name))
+        results.append(process_image_and_prompt(img_name))
+        df.loc[len(df.index)] = [img_name, process_image_and_prompt(img_name)]
+        data.to_csv("data_new.csv")
     
   finally:
       # Clean up and close the browser window
       driver.quit()
-  return screenshot_names
+  return df
+
+
+if __name__ == "__main__":
+  chromedriver_path = './chromedriver-mac-arm64/chromedriver'
+  cam_url = 'https://www.youtube.com/watch?v=Mez4F1iFxxM&ab_channel=CornellLabBirdCams'
+  iterations_count = 5
+  screenshots_frequency = 5
+  if not os.path.exists('screenshots'):
+    os.makedirs('screenshots')
+
+  screenshot_dir = 'screenshots'
+  # screenshot_names = [os.path.join(screenshot_dir, img) for img in os.listdir(screenshot_dir)]
+  # results = [process_image_and_prompt(img) for img in screenshot_names]
+
+
+  DIR = "data_new.csv"
+  if os.path.isfile(DIR):
+      data = pd.read_csv(DIR)
+      take_screenshot(data, cam_url, chromedriver_path, iterations_count, screenshots_frequency)  
+
+      # df_new = pd.DataFrame({"screenshots" : screenshot_names, "bird_tf": results})
+      # data = pd.concat([df, df_new])
+  else:
+      data = pd.DataFrame(columns=["screenshot_names", "bird_tf"])
+
+  # for img in os.listdir(DIR):
+  #     screenshot_names.append(img)
+  #     results.append(process_image_and_prompt(f"./screenshots/{img}"))
+  data.to_csv("data_new.csv")
+
+  df = pd.read_csv("data_new.csv")
+
+  df = df.loc[:, ~df.columns.str.contains('^Unnamed')]
+
+  df.to_csv("data.csv", index=False)
